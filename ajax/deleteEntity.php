26ba55d376594e65642655b945bceca75a4170b3
Edinei <galera.org@gmail.com>
2017-10-09 14:22:14 -0300

    delete entity relational

--- ajax/deleteEntity.php
+++ ajax/deleteEntity.php
@@ -1,13 +1,30 @@
 <?php
 $entityName = filter_input(INPUT_POST, 'entity', FILTER_DEFAULT);
 
-if(file_exists(PATH_HOME . "entity/{$entityName}.json")) {
+if (file_exists(PATH_HOME . "entity/{$entityName}.json")) {
+
+    $sql = new \ConnCrud\SqlCommand();
+    $entity = new \Entity\Entity($entityName);
+    $info = $entity->getJsonInfoEntity();
+    $dados = $entity->getJsonStructEntity();
+
+    if (!empty($info['extendMult'])) {
+        foreach ($info['extendMult'] as $item) {
+            $sql->exeCommand("DROP TABLE " . PRE . $entityName . "_" . $dados[$item]['table']);
+        }
+    }
+    if (!empty($info['listMult'])) {
+        foreach ($info['listMult'] as $item) {
+            $sql->exeCommand("DROP TABLE " . PRE . $entityName . "_" . $dados[$item]['table']);
+        }
+    }
+
+    $sql->exeCommand("DROP TABLE " . PRE . $entityName);
+
     unlink(PATH_HOME . "entity/{$entityName}.json");
     unlink(PATH_HOME . "entity/cache/{$entityName}.json");
     unlink(PATH_HOME . "entity/cache/{$entityName}_info.json");
 
-    $sql = new \ConnCrud\SqlCommand();
-    $sql->exeCommand("DROP TABLE " . PRE . $entityName);
 } else {
     echo "Entidade não encontrada";
 }
