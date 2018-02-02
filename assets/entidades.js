9bb489332eefa8cda97b2a5c64a2fea6e5dd6089
Edinei <galera.org@gmail.com>
2018-02-01 23:12:51 -0200

    alterações na input type e nome reservados ao sistema implementados

--- assets/entidades.js
+++ assets/entidades.js
@@ -48,7 +48,7 @@ function entityReset() {
 }
 
 function entityEdit(id) {
-    if((typeof(id) === "undefined" && entity.name !== "") || (typeof(id) !== "undefined" && id !== entity.name)) {
+    if ((typeof(id) === "undefined" && entity.name !== "") || (typeof(id) !== "undefined" && id !== entity.name)) {
         saveEntity(true);
         entityReset();
 
@@ -70,7 +70,11 @@ function showEntity() {
 
 function saveEntity(silent) {
     if (checkSaveAttr() && entity.name.length > 2 && typeof(dicionarios[entity.name]) !== "undefined" && !$.isEmptyObject(dicionarios[entity.name])) {
-        post("entity-form", "save/entity", {"name": entity.name, "dados": dicionarios[entity.name], "id": identifier[entity.name]}, function (g) {
+        post("entity-form", "save/entity", {
+            "name": entity.name,
+            "dados": dicionarios[entity.name],
+            "id": identifier[entity.name]
+        }, function (g) {
             if (g && typeof(silent) === "undefined") {
                 $("body").panel(themeNotify("Salvo"));
                 readDicionarios()
@@ -116,18 +120,18 @@ function checkSaveAttr() {
                         yes = false;
                     }
                 });
-                if(yes) {
+                if (yes && allowName(temp)) {
                     entity.name = temp;
                     identifier[entity.name] = 1;
                     dicionarios[entity.name] = {};
                 }
             }
-            if(yes) {
+            if (yes) {
                 entity.edit = identifier[entity.name];
                 identifier[entity.name]++;
             }
         }
-        if(yes) {
+        if (yes) {
             saveAttrInputs();
             resetAttr();
             showEntity();
@@ -295,11 +299,21 @@ function checkRequiresFields() {
     return (type !== "" && $("#nome").val().length > 1 && $("#nome").val() !== "id" && (["extend", "extend_mult", "list", "list_mult"].indexOf(type) < 0 || $("#relation").val() !== null));
 }
 
-function checkFieldsOpenOrClose() {
-    if (checkRequiresFields())
-        $(".requireName").removeClass("hide");
-    else
-        $(".requireName").addClass("hide");
+function checkFieldsOpenOrClose(nome) {
+    if(allowName(nome)) {
+        if (checkRequiresFields())
+            $(".requireName").removeClass("hide");
+        else
+            $(".requireName").addClass("hide");
+    }
+}
+
+function allowName(nome) {
+    if (["add", "all", "alter", "analyze", "and", "as", "asc", "asensitive", "before", "between", "bigint", "binary", "blob", "both", "by", "call", "cascade", "case", "change", "char", "character", "check", "collate", "column", "condition", "connection", "constraint", "continue", "convert", "create", "cross", "current_date", "current_time", "current_timestamp", "current_user", "cursor", "database", "databases", "day_hour", "day_microsecond", "day_minute", "day_second", "dec", "decimal", "declare", "default", "delayed", "delete", "desc", "describe", "deterministic", "distinct", "distinctrow", "div", "double", "drop", "dual", "each", "else", "elseif", "enclosed", "escaped", "exists", "exit", "explain", "false", "fetch", "float", "for", "force", "foreign", "from", "fulltext", "goto", "grant", "group", "having", "high_priority", "hour_microsecond", "hour_minute", "hour_second", "if", "ignore", "in", "index", "infile", "inner", "inout", "insensitive", "insert", "int", "integer", "interval", "into", "is", "iterate", "join", "key", "keys", "kill", "leading", "leave", "left", "like", "limit", "lines", "load", "localtime", "localtimestamp", "lock", "long", "longblob", "longtext", "loop", "low_priority", "match", "mediumblob", "mediumint", "mediumtext", "middleint", "minute_microsecond", "minute_second", "mod", "modifies", "natural", "not", "no_write_to_binlog", "null", "numeric", "on", "optimize", "option", "optionally", "or", "order", "out", "outer", "outfile", "precision", "primary", "procedure", "purge", "read", "reads", "real", "references", "regexp", "rename", "repeat", "replace", "require", "restrict", "return", "revoke", "right", "rlike", "schema", "schemas", "second_microsecond", "select", "sensitive", "separator", "set", "show", "smallint", "soname", "spatia", "specific", "sql", "sqlexception", "sqlstate", "sqlwarning", "sql_big_result", "sql_calc_found_rows", "sql_small_result", "ssl", "starting", "straight_join", "table", "terminated", "then", "tinyblob", "tinyint", "tinytext", "to", "trailing", "trigger", "true", "undo", "union", "unique", "unlock", "unsigned", "update", "usage", "use", "using", "utc_date", "utc_time", "utc_timestamp", "values", "varbinary", "varchar", "varcharacter", "varying", "when", "where", "while", "with", "write", "xor", "year_month", "zerofill"].indexOf(nome) > 0) {
+        $("body").panel(themeNotify("Este nome é reservado pelo sistema"));
+        return false;
+    }
+    return true;
 }
 
 function deleteAttr(id) {
@@ -362,7 +376,7 @@ $(".selectInput").change(function () {
 });
 
 $("#nome").on("keyup change", function () {
-    checkFieldsOpenOrClose();
+    checkFieldsOpenOrClose($(this).val());
 });
 
 $("#default_custom").change(function () {
