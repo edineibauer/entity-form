03b5b8b74ccdcec8f4a042a2313de2bd6353f899
Edinei <galera.org@gmail.com>
2018-10-02 16:32:54 -0300

    react update folders

--- src/EntityForm/React.php
+++ src/EntityForm/React.php
@@ -15,9 +15,9 @@ class React
      */
     public function __construct(string $action, string $entity, array $dados, array $dadosOld = [])
     {
-        $this->checkReact(PATH_HOME, $entity, $action, $dados, $dadosOld);
+        $this->checkReact(PATH_HOME . "public/", $entity, $action, $dados, $dadosOld);
 
-        foreach (Helper::listFolder(PATH_HOME . "vendor/conn") as $lib)
+        foreach (Helper::listFolder(PATH_HOME . VENDOR) as $lib)
             $this->checkReact(PATH_HOME . VENDOR . "{$lib}/", $entity, $action, $dados, $dadosOld);
     }
 
@@ -32,16 +32,18 @@ class React
      */
     private function checkReact(string $path, string $entity, string $acao, array $dados, array $dadosOld)
     {
-        if (file_exists("{$path}/entity/react/")) {
-            foreach (Helper::listFolder("{$path}/entity/react/") as $react) {
-                $actions = json_decode(file_get_contents("{$path}/entity/react/{$react}"), true);
+        if (file_exists("{$path}/react/")) {
+            foreach (Helper::listFolder("{$path}/react/") as $react) {
+                if(preg_match('/\.json$/i', $react)) {
+                    $actions = json_decode(file_get_contents("{$path}/entity/react/{$react}"), true);
 
-                if (is_array($actions) && !isset($actions['entity'])) {
-                    foreach ($actions as $ac)
-                        $this->loadReact($ac, $path, $acao, $entity, $dados, $dadosOld);
+                    if (is_array($actions) && !isset($actions['entity'])) {
+                        foreach ($actions as $ac)
+                            $this->loadReact($ac, $path, $acao, $entity, $dados, $dadosOld);
 
-                } else {
-                    $this->loadReact($actions, $path, $acao, $entity, $dados, $dadosOld);
+                    } else {
+                        $this->loadReact($actions, $path, $acao, $entity, $dados, $dadosOld);
+                    }
                 }
             }
         }
@@ -62,9 +64,9 @@ class React
         if(!empty($actions['entity']) && !empty($actions['action']) && !empty($actions['function'])) {
             if ((is_string($actions['entity']) && $actions['entity'] === $entity) || (is_array($actions['entity']) && in_array($entity, $actions['entity']))) {
                 if (is_string($actions['action']) && $actions['action'] === $action)
-                    include_once "{$path}/ajax/react/{$actions['function']}.php";
+                    include_once "{$path}/react/{$actions['function']}.php";
                 elseif (is_array($actions['action']) && in_array($action, $actions['action']))
-                    include_once "{$path}/ajax/react/{$actions['function']}.php";
+                    include_once "{$path}/react/{$actions['function']}.php";
             }
         }
     }
