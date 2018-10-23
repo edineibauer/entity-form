df097bdb1b489c831d21b0f5a10bd93937d07678
Edinei <galera.org@gmail.com>
2018-10-23 10:01:25 -0300

    toast implement

--- assets/entidades.js
+++ assets/entidades.js
@@ -201,7 +201,7 @@ function checkSaveAttr() {
                 let temp = slug($("#entityName").val(), '_');
                 $.each(dicionarios, function (nome, data) {
                     if (nome === temp) {
-                        $("body").panel(themeNotify("Nome de Entidade já existe", "warning"));
+                        toast("Nome de Entidade já existe", 2000, "toast-warning");
                         yes = false;
                     }
                 });
@@ -489,7 +489,7 @@ function allowName(nome, tipo) {
     if (typeof nome !== "undefined") {
         //nomes especiais do banco
         if (["add", "all", "alter", "analyze", "and", "as", "asc", "asensitive", "before", "between", "bigint", "binary", "blob", "both", "by", "call", "cascade", "case", "change", "char", "character", "check", "collate", "column", "condition", "connection", "constraint", "continue", "convert", "create", "cross", "current_date", "current_time", "current_timestamp", "current_user", "cursor", "database", "databases", "day_hour", "day_microsecond", "day_minute", "day_second", "dec", "decimal", "declare", "default", "delayed", "delete", "desc", "describe", "deterministic", "distinct", "distinctrow", "div", "double", "drop", "dual", "each", "else", "elseif", "enclosed", "escaped", "exists", "exit", "explain", "false", "fetch", "float", "for", "force", "foreign", "from", "fulltext", "goto", "grant", "group", "having", "high_priority", "hour_microsecond", "hour_minute", "hour_second", "if", "ignore", "in", "index", "infile", "inner", "inout", "insensitive", "insert", "int", "integer", "interval", "into", "is", "iterate", "join", "key", "keys", "kill", "leading", "leave", "left", "like", "limit", "lines", "load", "localtime", "localtimestamp", "lock", "long", "longblob", "longtext", "loop", "low_priority", "match", "mediumblob", "mediumint", "mediumtext", "middleint", "minute_microsecond", "minute_second", "mod", "modifies", "natural", "not", "no_write_to_binlog", "null", "numeric", "on", "optimize", "option", "optionally", "or", "order", "out", "outer", "outfile", "precision", "primary", "procedure", "purge", "read", "reads", "real", "references", "regexp", "rename", "repeat", "replace", "require", "restrict", "return", "revoke", "right", "rlike", "schema", "schemas", "second_microsecond", "select", "sensitive", "separator", "set", "show", "smallint", "soname", "spatia", "specific", "sql", "sqlexception", "sqlstate", "sqlwarning", "sql_big_result", "sql_calc_found_rows", "sql_small_result", "ssl", "starting", "straight_join", "table", "terminated", "then", "tinyblob", "tinyint", "tinytext", "to", "trailing", "trigger", "true", "undo", "union", "unique", "unlock", "unsigned", "update", "usage", "use", "using", "utc_date", "utc_time", "utc_timestamp", "values", "varbinary", "varchar", "varcharacter", "varying", "when", "where", "while", "with", "write", "xor", "year_month", "zerofill"].indexOf(nome) > 0) {
-            $("body").panel(themeNotify("Este nome é reservado pelo sistema", "error"));
+            toast("Este nome é reservado pelo sistema", 3000, "toast-error");
             $(".requireName").addClass("hide");
             return false;
         }
@@ -542,7 +542,7 @@ function removeEntity(entity) {
     if (confirm("Excluir esta entidade e todos os seus dados?")) {
         post("entity-form", "delete/entity", {"name": entity}, function (g) {
             if (g) {
-                $("body").panel(themeNotify("Entidade Excluída", "warning"));
+                toast("Entidade Excluída", 3000, "toast-warning");
                 readDicionarios();
             }
         })
