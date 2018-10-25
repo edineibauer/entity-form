46c73db032085165db96550f58a40e7af8d9dfa8
Edinei <galera.org@gmail.com>
2018-10-25 20:01:54 -0300

    react com setor

--- src/EntityForm/React.php
+++ src/EntityForm/React.php
@@ -19,9 +19,16 @@ class React
         if(file_exists(PATH_HOME . "public/react/{$entity}/{$action}.php"))
             include PATH_HOME . "public/react/{$entity}/{$action}.php";
 
+        /* CRUD REACT SETOR DEFAULT */
+        if(file_exists(PATH_HOME . "public/react/{$entity}/{$_SESSION['userlogin']['setor']}/{$action}.php"))
+            include PATH_HOME . "public/react/{$entity}/{$_SESSION['userlogin']['setor']}/{$action}.php";
+
         foreach (Helper::listFolder(PATH_HOME . VENDOR) as $lib){
             if(file_exists(PATH_HOME . VENDOR . "{$lib}/react/{$entity}/{$action}.php"))
                 include PATH_HOME . VENDOR . "{$lib}/react/{$entity}/{$action}.php";
+
+            if(file_exists(PATH_HOME . VENDOR . "{$lib}/react/{$entity}/{$_SESSION['userlogin']['setor']}/{$action}.php"))
+                include PATH_HOME . VENDOR . "{$lib}/react/{$entity}/{$_SESSION['userlogin']['setor']}/{$action}.php";
         }
     }
 }
